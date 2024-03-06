a036d6359434c3323d0dbcbfe19e5630db6a28e0
Marek Samofal <marek.samofal@gmail.com>
2024-03-06 14:36:06 +0100

    Controlled elements 03

--- src/App.js
+++ src/App.js
@@ -23,24 +23,30 @@ function Logo() {
 
 function Form() {
 	const [description, setDescription] = useState('')
-	const [number, setNumber] = useState('')
+	const [quantity, setQuantity] = useState(1)
 
 	function handleSubmit(e) {
 		e.preventDefault()
-		console.log(e.target)
+
+		if (!description) return
+
+		const newItem = { description, quantity, packed: false, id: Date.now() }
+
+		setDescription('')
+		setQuantity(1)
 	}
 
 	return (
 		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your 🥴 trip?</h3>
-			<select value={number} onChange={e => setNumber(e.target.value)}>
+			<select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type='text' placeholder='Item...' value={description} onChange={e => setDescription(e.target.value)} />
+			<input type='text' placeholder='Item...' value={description} onChange={e => setDescription(e.newItem)} />
 			<button>Add</button>
 		</form>
 	)
