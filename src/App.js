6b05407c4a035377a28965177ab71b12d699fa29
Marek Samofal <marek.samofal@gmail.com>
2024-03-06 13:50:08 +0100

    controlled elements 02

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function Logo() {
 
 function Form() {
 	const [description, setDescription] = useState('')
+	const [number, setNumber] = useState('')
 
 	function handleSubmit(e) {
 		e.preventDefault()
@@ -32,14 +33,14 @@ function Form() {
 	return (
 		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your 🥴 trip?</h3>
-			<select>
+			<select value={number} onChange={e => setNumber(e.target.value)}>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type='text' placeholder='Item...' />
+			<input type='text' placeholder='Item...' value={description} onChange={e => setDescription(e.target.value)} />
 			<button>Add</button>
 		</form>
 	)
