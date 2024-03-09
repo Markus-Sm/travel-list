dd917c60febdec5c6315357b62471bf557631b6c
Marek Samofal <marek.samofal@gmail.com>
2024-03-09 20:35:33 +0100

    docs: adding new state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 
-const initialItems = [
-	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-	{ id: 2, description: 'Socks', quantity: 12, packed: false },
-	{ id: 3, description: 'Charger', quantity: 12, packed: true },
-]
+// const initialItems = [
+// 	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+// 	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+// 	{ id: 3, description: 'Charger', quantity: 12, packed: true },
+// ]
 
 export default function App() {
+	const [items, setItems] = useState([])
+
+	function handleAddItems(item) {
+		setItems(items => [...items, item])
+	}
+
 	return (
 		<div className='app'>
 			<Logo />
-			<Form />
-			<PackingList />
+			<Form onAddItems={handleAddItems} />
+			<PackingList items={items} />
 			<Stats />
 		</div>
 	)
@@ -21,7 +27,7 @@ function Logo() {
 	return <h1>🏝️ Far Away 🧳</h1>
 }
 
-function Form() {
+function Form({ onAddItems }) {
 	const [description, setDescription] = useState('')
 	const [quantity, setQuantity] = useState(1)
 
@@ -31,6 +37,8 @@ function Form() {
 		if (!description) return
 
 		const newItem = { description, quantity, packed: false, id: Date.now() }
+
+		onAddItems(newItem)
 
 		setDescription('')
 		setQuantity(1)
@@ -46,17 +54,17 @@ function Form() {
 					</option>
 				))}
 			</select>
-			<input type='text' placeholder='Item...' value={description} onChange={e => setDescription(e.newItem)} />
+			<input type='text' placeholder='Item...' value={description} onChange={e => setDescription(e.target.value)} />
 			<button>Add</button>
 		</form>
 	)
 }
 
-function PackingList() {
+function PackingList({ items }) {
 	return (
 		<div className='list'>
 			<ul>
-				{initialItems.map(item => (
+				{items.map(item => (
 					<Item item={item} key={item.id} />
 				))}
 			</ul>
