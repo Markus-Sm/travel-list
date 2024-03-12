7461055fa34fdee2bc73af2e747b9c4dc1b5ed2a
Marek Samofal <marek.samofal@gmail.com>
2024-03-12 19:07:26 +0100

    docs: update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
-// const initialItems = [
-// 	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-// 	{ id: 2, description: 'Socks', quantity: 12, packed: false },
-// 	{ id: 3, description: 'Charger', quantity: 12, packed: true },
-// ]
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+	{ id: 3, description: 'Charger', quantity: 12, packed: false },
+]
 
 export default function App() {
-	const [items, setItems] = useState([])
+	const [items, setItems] = useState(initialItems)
 
 	function handleAddItems(item) {
 		setItems(items => [...items, item])
@@ -101,7 +101,7 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 function Stats() {
 	return (
 		<footer className='stats'>
-			<em>💼 You have X! items on your list, and you already packed X (X%)</em>
+			<em>💼 You have X items on your list, and you already packed X (X%)</em>
 		</footer>
 	)
 }
